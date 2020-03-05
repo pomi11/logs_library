2506b34d01d09e62a21fe3446fa2666e93e04c47
pomi11 <pomidor1992@gmail.com>
2020-03-05 13:52:00 +0100

    dodanie klasy LOG

--- logs_library.cpp
+++ logs_library.cpp
@@ -2,5 +2,36 @@
 
 LOG::LOG()
 {
-    std::cout<<"To dziala!";
+    date=QDateTime::currentDateTime();
+}
+
+LOG::LOG(std::string header,std::string message,std::vector<std::pair<std::string,std::string>> custom)
+{
+    date=QDateTime::currentDateTime();
+    sHeader=header;
+    sMessage=message;
+    vCustoms=custom;
+}
+
+void LOG::add_custom(std::string tag, std::string message)
+{
+    vCustoms.push_back(std::pair<std::string,std::string>(tag,message));
+}
+
+void LOG::add_customs(std::vector<std::pair<std::string,std::string>> custom)
+{
+    foreach(auto item,custom)
+    {
+        vCustoms.push_back(item);
+    }
+}
+
+void LOG::set_date(std::string date,std::string format)
+{
+    this->date=QDateTime::fromString(QString::fromStdString(date),QString::fromStdString(format));
+}
+
+LOGS::LOGS()
+{
+
 }
--- logs_library.h
+++ logs_library.h
@@ -2,12 +2,42 @@
 #define LOGS_LIBRARY_H
 
 #include "logs_library_global.h"
+#include <QDateTime>
 #include <iostream>
+#include <vector>
+#include <map>
 
 class LOGS_LIBRARY_EXPORT LOG
 {
+private:
+    std::string sHeader,sMessage;
+    QDateTime date;
+    std::vector<std::pair<std::string,std::string>> vCustoms;
+
 public:
     LOG();
+    LOG(QDateTime date) {this->date=date;};
+    LOG(std::string header,std::string message,std::vector<std::pair<std::string,std::string>> custom);
+    void set_message(std::string message)   {this->sMessage=message;};
+    void set_customs(std::vector<std::pair<std::string,std::string>> custom) {vCustoms=custom;};
+    void add_custom(std::string tag, std::string message);
+    void add_customs(std::vector<std::pair<std::string,std::string>> custom);
+    void set_date(std::string date,std::string format);
+    void set_date(QDateTime date)   {this->date=date;};
+};
+
+/*class LOGS_LIBRARY_EXPORT FILE_STUCT
+{
+
+};
+*/
+
+class LOGS_LIBRARY_EXPORT LOGS
+{
+private:
+    std::string login;
+public:
+    LOGS();
 };
 
 #endif // LOGS_LIBRARY_H
