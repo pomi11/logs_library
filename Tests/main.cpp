1e2c69da4b5d22d8af04ac1fa89ef9156d3c1ea6
pomi11 <pomidor1992@gmail.com>
2020-03-05 13:53:46 +0100

    testy...

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,9 +1,15 @@
 #include <QCoreApplication>
 #include <../logs_library.h>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    LOG log;
+    QDateTime qdt = QDateTime::currentDateTime();
+
+
+    QDateTime afw = QDateTime::fromString(QDateTime::currentDateTime().toString("dd:MMMM:yyyy//HH:mm:ss"),"dd:MMMM:yyyy//HH:mm:ss");
+    QDate dd= QDate::fromString()
+    qDebug()<<afw;
     return a.exec();
 }
